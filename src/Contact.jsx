f8cce9752a291d1c74b31b5c0b605525dc5829db
tousifk436-coder <tousifk436@gmail.comm>
2026-04-03 10:07:40 +0530

    add again

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -88,7 +88,7 @@ function Contact() {
               >
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
-              <span>+91-6388774338</span>
+              <span>+91-6392333430</span>
             </p>
             <p className="flex items-center">
               <svg
@@ -100,7 +100,7 @@ function Contact() {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
               </svg>
-              <span>sanjay892000@gmail.com.com</span>
+              <span>tousifk436@gmail.com</span>
             </p>
           </div>
         </div>
